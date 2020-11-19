69236f98305f61734d067bf58622d0801a38bc2d
dineshneela <70243554+dineshneela@users.noreply.github.com>
2020-11-19 17:00:20 -0500

    Add files via upload

---
+++ js/Food.js
@@ -0,0 +1,31 @@
+ class Food {
+     constructor(foodstock,lastfed){
+     this.image=loadImage("milk.png")
+     }
+     getfoodstock(){
+
+     }
+     updatefoodstock(){
+
+     }
+     deductfoodstock(){
+
+     }
+     display(){
+         var x=80,y=100
+         imageMode(CENTER)
+         image(this.image,720,200,70,70)
+         if(this.foodstock!=0){
+             for(var i=0;i<this.foodstock;i++){
+                 if(1%10==0){
+                     x=80
+                     y=y+80
+
+                 }
+                  image(this.image,x,y,50,50)
+                  x=x+30;
+             }
+         }
+     }
+
+ }
